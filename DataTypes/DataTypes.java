8525e1711bf6ca1ee198d15f39f546103a23b611
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2023-11-29 15:53:10 +0530

    Add files via upload
    Understanding data types

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class DataTypes {
+    public static void main(String[] args) {
+/*
+           Primitive Data Types
+1.Boolean
+2. Character (char)
+3.Integer = a)byte b)Short c)int d)long
+In floating point there are two types of integral values such as
+1.Float
+2.double
+ lets understand with code
+ */
+        boolean a = true;
+        char ch = 'J';
+        byte rollNo = 22;
+        short age = 23;
+        int age1 = 34;
+        long l = 365645643476l;
+        float f = 45.89f;
+        double d = 67.98;
+        System.out.println(l);
+
+    }
+}
